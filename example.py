a84439c4b471cdcd2d3b3c86db9aa1ffea2080c2
RMitchellHughes <kingofnintendo@gmail.com>
2024-08-07 03:22:26 -0600

    Update example.py

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from BB_confound import BB_confound
 
 def main():
     # Read in data and divide between x, y, and s
-    dat = pd.read_csv('/Users/mitchell/Documents/NHANES Project/NHANES_07_12.csv')
+    dat = pd.read_csv('NHANES_07_12.csv')
 
     x = dat['SD.level']
     y = dat['bmi']
