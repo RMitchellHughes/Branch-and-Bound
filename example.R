02a43ebadc2e5a2e1a5528870d226947072b4de4
RMitchellHughes <kingofnintendo@gmail.com>
2024-08-06 22:34:27 -0600

    Update example.R

--- example.R
+++ example.R
@@ -1,28 +1,27 @@
 # An example application of our algorithm (see section 4)
 
-# Read in data and divide between x, y, and w
+# Read in data and divide between x, y, and s
 dat <- read.csv("NHANES_07_12.csv")
 dat.x <- dat$SD.level
 dat.y <- dat$bmi
-dat.w <- dat[c('age','gender','fish.score','vitD.supplement','MET.score','white','black')]
+dat.s <- dat[c('age','gender','fish.score','vitD.supplement','MET.score','white','black')]
 
 # Create interaction terms
-p <- ncol(dat.w)
+p <- ncol(dat.s)
 for(var1 in 1:p) {
   for (var2 in var1:p) {
     if (var1 == var2) {
       next
     } else {
-      var.name <- paste0(colnames(dat.w)[c(var1, var2)], collapse = '_')
+      var.name <- paste0(colnames(dat.s)[c(var1, var2)], collapse = '_')
     }
-    dat.w[var.name] <- dat.w[var1] * dat.w[var2]
+    dat.s[var.name] <- dat.s[var1] * dat.s[var2]
   }
 }
 # Eliminate interaction between racial indicator variables
-dat.w <- subset(dat.w, select = !(colnames(dat.w) %in% 'white_black'))
+dat.s <- subset(dat.s, select = !(colnames(dat.s) %in% 'white_black'))
 
 # Run the BBR algorithm
-BB.confound.reorder(dat.x, dat.y, dat.w)
+BB.confound(dat.x, dat.y, dat.s)
 
 # Note: Because the data set is large, it may take a while for the algorithm to finish.
-# In fact, the BB.confound algorithm never finished after 200 minutes of runtime.
